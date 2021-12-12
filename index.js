d0f43794a688af645e6d5f08dfd6c1bfe49f9188
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2021-12-12 10:50:55 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let thankYou = [];
+for (let i = 0; i<names.length; i++) {
+    thankYou.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`); 
+}
+    return thankYou;
+}
+
+
+function countDown(num){
+    let i = num;
+    while ( i >= 0 ){
+        console.log (i--);
+    }
+}
