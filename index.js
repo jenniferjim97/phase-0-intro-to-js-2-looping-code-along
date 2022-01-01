336f4070754fbda27cd3c2bb92d375e0f821d4e6
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2022-01-01 11:40:19 -0500

    commit tree 10f596413a28851704edca119c1fd5f8e1578fae

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-function writeCards(names, event) {
+function writeCards(names,event){
     let thankYou = [];
-for (let i = 0; i<names.length; i++) {
-    thankYou.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`); 
-}
+    for(let i = 0; i < names.length; i++) {
+        thankYou.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
     return thankYou;
 }
 
-
 function countDown(num){
-    let i = num;
-    while ( i >= 0 ){
-        console.log (i--);
-    }
+let i = num;
+while (i >= 0){
+console.log(i--);
+}
 }
